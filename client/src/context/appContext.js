72634527d5769776b551672118bd1395bb75f538
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-12-03 11:58:47 +0530

    clear alert

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,6 +1,6 @@
 import React, { useReducer, useContext } from 'react';
 import reducer from './reducer';
-import { DISPLAY_ALERT } from "./action";
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./action";
 
 const initialState = {
 	isLoading: false,
@@ -14,10 +14,20 @@ const AppContext = React.createContext()
 const AppProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState)
 
+	const clearAlert = () => {
+		setTimeout(() => {
+			dispatch({
+				type: CLEAR_ALERT
+			})
+		}, 3000);
+
+	}
+
 	const displayAlert = () => {
 		dispatch({
 			type: DISPLAY_ALERT
 		})
+		clearAlert()
 	}
 
 	return (
