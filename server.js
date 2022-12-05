12e3b188ced96a5ad17e11bb4fd79bfca0cf987c
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-12-05 13:01:08 +0530

    setup server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Server running...')
+import express from "express";
+const app = express();
+
+app.get('/', (req, res)=> {
+	res.send('Welcome!')
+})
+
+const port = process.env.port || 5000;
+
+app.listen(port, ()=>{
+	console.log(`Server is listening at ${port}...`);
+})
