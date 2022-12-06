5527a7ca2249a8075bc692b1e4dd34e88c891039
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-12-06 12:40:59 +0530

    add .env file

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 import errorHandlerMiddleware from "./middleware/error-handler.js";
@@ -14,7 +16,7 @@ app.get('/', (req, res)=> {
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
 	console.log(`Server is listening at ${port}...`);
