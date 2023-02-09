27ca1d75245f34b55b1d77452726d6641d7b9c3f
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-02-09 11:27:19 +0530

    add login functionality

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -6,7 +6,10 @@ import {
 	CLEAR_ALERT,
 	REGISTER_USER_BEGIN,
 	REGISTER_USER_SUCCESS,
-	REGISTER_USER_ERROR
+	REGISTER_USER_ERROR,
+	LOGIN_USER_BEGIN,
+	LOGIN_USER_SUCCESS,
+	LOGIN_USER_ERROR
 } from "./action";
 
 const user = localStorage.getItem('user')
@@ -88,8 +91,12 @@ const AppProvider = ({ children }) => {
 		clearAlert()
 	}
 
+	const loginUser = async (currentUser) => {
+		console.log(currentUser);
+	}
+
 	return (
-		<AppContext.Provider value={{ ...state, displayAlert, registerUser }}>
+		<AppContext.Provider value={{ ...state, displayAlert, registerUser, loginUser }}>
 			{children}
 		</AppContext.Provider>
 	)
