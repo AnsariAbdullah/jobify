35cc8c66c2548c225b4a28b6bfa28633149330e3
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-02-13 13:13:30 +0530

    add nested routes

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,13 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<div>Dashboard</div>} />
+				{/* setting nested routes */}
+				<Route path='/'>
+					<Route path='add-jobs' element={<AddJob />} />
+					<Route path='all-jobs' element={<AllJobs />} />
+					<Route path='profile' element={<Profile />} />
+					<Route path='stats' element={<Stats />} />
+				</Route>
 				<Route path='/register' element={<Register />} />
 				<Route path='/landing' element={<Landing />} />
 				<Route path='*' element={<Error />} />
