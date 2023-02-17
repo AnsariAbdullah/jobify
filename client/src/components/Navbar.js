6819c298281eeb3ff85df5a2a7864cda97d78f2d
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-02-17 15:06:57 +0530

    add icon in nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,14 @@
 import Wrapper from "../assets/wrappers/Navbar";
+import { FaHome } from 'react-icons/fa';
+
 
 const Navbar = () => {
 	return (
 		<Wrapper>
 			<h1>Nav bar</h1>
+			<FaHome />
 		</Wrapper>
 	);
 }
- 
+
 export default Navbar;
