6e6f4e839a90f0216c3f94640b0a6194d5522b13
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-03-15 07:31:21 +0530

    clean up

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
-const auth = async (req, res, next) => {
-	console.log('authenticate user');
+const auth = async (req, res, next) => {;
+	const authorization = req.headers.authorization;
+	console.log(authorization);
 	next()
 }
 
