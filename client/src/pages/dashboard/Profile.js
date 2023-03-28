54b66756c680462949600799da557a7a65cc02df
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-03-28 11:11:54 +0530

    add structure and states

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,7 +1,65 @@
+import { useState } from 'react';
+import { FormRow, Alert } from '../../components';
+import { useAppContext } from '../../context/appContext';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+
 const Profile = () => {
+
+	const { user, showAlert, displayAlert, updateUser, isLoading } = useAppContext();
+
+	const [name, setName] = useState(user?.name)
+  const [email, setEmail] = useState(user?.email)
+  const [lastName, setLastName] = useState(user?.lastName)
+  const [location, setLocation] = useState(user?.location)
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('update user');
+	}
+
 	return (
-		<h1>Profile pages</h1>
+		<Wrapper>
+			<form onSubmit={handleSubmit} className='form'>
+				<h3>Profile</h3>
+				{showAlert && <Alert />}
+				<div className='form-center'>
+					<FormRow 
+						type="text"
+						name="name"
+						value={name}
+						onChange={(e)=> setName(e.target.value)}
+					/>
+
+					<FormRow 
+						type="text"
+						labelText='last name'
+						name="lastName"
+						value={lastName}
+						onChange={(e)=> setLastName(e.target.value)}
+					/>
+
+					<FormRow 
+						type="email"
+						name="email"
+						value={email}
+						onChange={(e)=> setEmail(e.target.value)}
+					/>
+
+					<FormRow 
+						type="text"
+						name="location"
+						value={location}
+						onChange={(e)=> setLocation(e.target.value)}
+					/>
+
+					<button className='btn btn-block' type='submit' disabled={isLoading}>
+						{isLoading ? 'Please wait...' : 'Save changes'}
+					</button>
+
+				</div>
+			</form>
+		</Wrapper>
 	);
 }
- 
+
 export default Profile;
