c2d38eaabb8a7872998a45b3c0ebd31f372f6550
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-03-29 11:14:15 +0530

    fix formatting

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -8,13 +8,13 @@ const Profile = () => {
 	const { user, showAlert, displayAlert, updateUser, isLoading } = useAppContext();
 
 	const [name, setName] = useState(user?.name)
-  const [email, setEmail] = useState(user?.email)
-  const [lastName, setLastName] = useState(user?.lastName)
-  const [location, setLocation] = useState(user?.location)
+	const [email, setEmail] = useState(user?.email)
+	const [lastName, setLastName] = useState(user?.lastName)
+	const [location, setLocation] = useState(user?.location)
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		if(!name || !email || !lastName || !location){
+		if (!name || !email || !lastName || !location) {
 			displayAlert()
 			return
 		}
@@ -27,33 +27,33 @@ const Profile = () => {
 				<h3>Profile</h3>
 				{showAlert && <Alert />}
 				<div className='form-center'>
-					<FormRow 
+					<FormRow
 						type="text"
 						name="name"
 						value={name}
-						handleChange={(e)=> setName(e.target.value)}
+						handleChange={(e) => setName(e.target.value)}
 					/>
 
-					<FormRow 
+					<FormRow
 						type="text"
 						labelText='last name'
 						name="lastName"
 						value={lastName}
-						handleChange={(e)=> setLastName(e.target.value)}
+						handleChange={(e) => setLastName(e.target.value)}
 					/>
 
-					<FormRow 
+					<FormRow
 						type="email"
 						name="email"
 						value={email}
-						handleChange={(e)=> setEmail(e.target.value)}
+						handleChange={(e) => setEmail(e.target.value)}
 					/>
 
-					<FormRow 
+					<FormRow
 						type="text"
 						name="location"
 						value={location}
-						handleChange={(e)=> setLocation(e.target.value)}
+						handleChange={(e) => setLocation(e.target.value)}
 					/>
 
 					<button className='btn btn-block' type='submit' disabled={isLoading}>
