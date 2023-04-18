5f5b1be295dd8ee08a91550f8f0983564072b147
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-04-18 15:56:27 +0530

    add jobtype select

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -65,6 +65,25 @@ const AddJob = () => {
 					/>
 					{/* job type */}
 
+					<div className='form-row'>
+						<label htmlFor='jobType' className='form-label'>
+							job type
+						</label>
+						<select
+							name='jobType'
+							value={jobType}
+							onChange={handleJobInput}
+							className='form-select'
+						>
+							{jobTypeOptions.map((itemValue, index)=>
+								<option key={index} value={itemValue}>
+									{itemValue}
+								</option>
+							)}
+						</select>
+
+					</div>
+
 					{/* job status */}
 
 					<div className='btn-container'>
