a9df79764ac3c6dc4d60cd767cdf78e968f4a9f4
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-05-17 09:14:17 +0530

    add icons and text in job info

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -18,7 +18,7 @@ const Job = ({
 	const { setEditJob, deleteJob } = useAppContext();
 
 	let date = moment(createdAt);
-  date = date.format('MMM Do, YYYY');
+	date = date.format('MMM Do, YYYY');
 	return (
 		<Wrapper>
 			<header>
@@ -29,6 +29,12 @@ const Job = ({
 				</div>
 			</header>
 			<div className='content'>
+				<div className='content-center'>
+					<JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+					<JobInfo icon={<FaCalendarAlt />} text={date} />
+					<JobInfo icon={<FaBriefcase />} text={jobType} />
+					<div className={`status ${status}`}>{status}</div>
+				</div>
 				<footer>
 					<div className='actions'>
 						<Link
