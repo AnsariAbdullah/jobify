02be33c5a59100b0f0a82fddf7c799589c95781c
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-07-17 11:53:09 +0530

    complete statsItem component

--- client/src/components/StatsItem.js
+++ client/src/components/StatsItem.js
@@ -1,8 +1,14 @@
-const StatsItem = () => {
+import Wrapper from "../assets/wrappers/StatItem";
+
+const StatsItem = ({ title, count, icon, color, bcg }) => {
 	return (
-		<h1>
-			Single stats item
-		</h1>
+		<Wrapper color={color} bcg={bcg}>
+			<header>
+				<span className="count">{count}</span>
+				<div className="icon">{icon}</div>
+			</header>
+			<h5 className="title">{title}</h5>
+		</Wrapper>
 	);
 }
  
