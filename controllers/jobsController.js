d3cded31af8e43b710735edd625914dff4ac8bdd
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-08-02 11:39:00 +0530

    add query for status param

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -19,7 +19,19 @@ const createJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
-	const jobs = await Job.find({ createdBy: req.user.userId })
+	const { status, jobType, sort, search } = req.query;
+
+	const queryObject = {
+		createdBy: req.user.userId
+	}
+
+	if(status !=='all'){
+		queryObject.status = status;
+	}
+
+	let result =  Job.find(queryObject)
+	
+	const jobs = await result
 
 	res.status(StatusCodes.OK).json({
 		jobs,
