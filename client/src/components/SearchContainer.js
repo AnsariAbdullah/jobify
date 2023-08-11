157c5e85649deed99ee05f2fc7fd600434622504
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-08-11 09:20:31 +0530

    add search box

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { FormRow, FormRowSelect } from '.';
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/SearchContainer';
+
 
 
 const SearchContainer = () => {
+	const {
+    isLoading,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+    sortOptions,
+    statusOptions,
+    jobTypeOptions,
+    handleChange,
+    clearFilters,
+  } = useAppContext();
+
+	const handleSearch = (e) => {
+		if(isLoading) return
+		handleChange({name: e.target.name, value: e.target.value})
+	}
+
 	return (
-		<h1>
-			Search container
-		</h1>
+		<Wrapper>
+			<form className='form'>
+				<h4>Search form</h4>
+				<div className='form-center'>
+					<FormRow
+						type='text'
+						name='search'
+						value={search}
+						handleChange={handleSearch}
+					/>
+				</div>
+			</form>
+		</Wrapper>
 	);
 }
 
