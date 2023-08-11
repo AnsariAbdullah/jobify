c13c50d7ebd02476f8617399a4a80b80682066d1
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-08-11 09:20:55 +0530

    format document

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -7,21 +7,21 @@ import Wrapper from '../assets/wrappers/SearchContainer';
 
 const SearchContainer = () => {
 	const {
-    isLoading,
-    search,
-    searchStatus,
-    searchType,
-    sort,
-    sortOptions,
-    statusOptions,
-    jobTypeOptions,
-    handleChange,
-    clearFilters,
-  } = useAppContext();
+		isLoading,
+		search,
+		searchStatus,
+		searchType,
+		sort,
+		sortOptions,
+		statusOptions,
+		jobTypeOptions,
+		handleChange,
+		clearFilters,
+	} = useAppContext();
 
 	const handleSearch = (e) => {
-		if(isLoading) return
-		handleChange({name: e.target.name, value: e.target.value})
+		if (isLoading) return
+		handleChange({ name: e.target.name, value: e.target.value })
 	}
 
 	return (
