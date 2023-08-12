f6c022c58ab1f8b4c16c40a385d23ee1c55ac50f
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-08-12 13:11:51 +0530

    add status, type and sort filter functionality

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -24,17 +24,53 @@ const SearchContainer = () => {
 		handleChange({ name: e.target.name, value: e.target.value })
 	}
 
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		clearFilters()
+	}
+
 	return (
 		<Wrapper>
 			<form className='form'>
 				<h4>Search form</h4>
 				<div className='form-center'>
+					{/* search position */}
 					<FormRow
 						type='text'
 						name='search'
 						value={search}
 						handleChange={handleSearch}
 					/>
+					{/* search by status */}
+					<FormRowSelect
+						labelText='status'
+						name='searchStatus'
+						value={searchStatus}
+						handleChange={handleSearch}
+						list={['all', ...statusOptions]}
+					/>
+					{/* search by status */}
+					<FormRowSelect
+						labelText='job type'
+						name='searchType'
+						value={searchType}
+						handleChange={handleSearch}
+						list={['all', ...jobTypeOptions]}
+					/>
+					{/* sort */}
+					<FormRowSelect
+						name='sort'
+						value={sort}
+						handleChange={handleSearch}
+						list={sortOptions}
+					/>
+					<button
+						className='btn btn-block btn-danger'
+						disabled={isLoading}
+						onClick={handleSubmit}
+					>
+						clear filters
+					</button>
 				</div>
 			</form>
 		</Wrapper>
