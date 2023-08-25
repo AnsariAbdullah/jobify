fa1f92637593fa3b6f398f73da7cd71a91ec81db
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-08-25 14:27:47 +0530

    add components and functionality in PageBtnContainer

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -1,6 +1,35 @@
+import { useAppContext } from "../context/appContext";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
+import Wrapper from '../assets/wrappers/PageBtnContainer';
+
 const PageBtnContainer = () => {
+	const { numOfPages, page } = useAppContext();
+
+	const prevPage = () => {
+		console.log('prev page');
+	}
+
+	const nextPage = ()  => {
+		console.log('next page');
+	}
+
 	return (
-		<h1>PageBtnContainer</h1>
+		<Wrapper>
+			<button className="prev-btn" onClick={prevPage}>
+				<HiChevronDoubleLeft />
+				prev
+			</button>
+
+			<div className="btn-container">
+				buttons
+			</div>
+
+			<button className="next-btn" onClick={nextPage}>
+				<HiChevronDoubleRight />
+				next
+			</button>
+
+		</Wrapper>
 	);
 }
  
