3da529eefdd98aef071d070ef0b4fc6324c82c7e
AnsariAbdullah <ansariabdullahar@gmail.com>
2023-09-11 11:38:42 +0530

    add security packages

--- server.js
+++ server.js
@@ -21,14 +21,22 @@ import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 import authenticateUser from './middleware/auth.js';
 
+// secutiry packages
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
+
 if(process.env.NODE_ENV !== 'production'){
 	app.use(morgan('dev'))
 }
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-app.use(express.static(path.resolve(__dirname, './client/build')))
-app.use(express.json())
+app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.get('/', (req, res) => {
 	res.send('Welcome!');
