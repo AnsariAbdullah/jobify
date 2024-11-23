4c10d251e908958f85f090ad61bfce4df16fd27b
AnsariAbdullah <ansariabdullahar@gmail.com>
2024-11-23 17:21:04 +0530

    add validation

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import mongoose from 'mongoose';
+import { body, validationResult } from 'express-validator';
 
 // routers
 import jobRouter from './routes/jobRouter.js';
@@ -23,11 +24,28 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 })
 
-app.post('/', (req, res) => {
-  console.log(req);
+app.post('/api/v1/test',
+  [
+    body('name')
+      .notEmpty()
+      .withMessage('name is required')
+      .isLength({ min: 50 })
+      .withMessage('name should be more than 50')
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      const errorMessage = errors.array().map((err) => err.msg);
+      return res.status(400).json({ errors: errorMessage });
+    }
+    next()
+  },
+  (req, res) => {
+    const { name } = req.body;
 
-  res.json({ message: 'data recieved', data: req.body })
-})
+    res.json({ message: `hello ${name}` });
+  })
 
 app.use('/api/v1/jobs', jobRouter)
 
