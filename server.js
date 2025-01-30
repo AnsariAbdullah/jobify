ab1a70a71ecf8b928dd33902905257c6e7724ba3
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-01-30 16:46:06 +0530

    add autheticateUser middleware

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import authRouter from './routes/authRouter.js';
 
 // middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import { authenticateUser } from './middleware/authMiddleware.js';
 
 
 if (process.env.NODE_ENV === 'development') {
@@ -24,7 +25,7 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 })
 
-app.use('/api/v1/jobs', jobRouter);
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 
 // NOT FOUND
