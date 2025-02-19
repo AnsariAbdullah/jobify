512b21ec1318955c70ba492cd8c199eb4deaf1f4
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-02-19 17:18:39 +0530

    add verify JWT functionality

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,15 @@
 import { UnauthenticatedError } from "../errors/customErrors.js";
+import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = async (req, res, next) => {
   const { token } = req.cookies;
   // console.log(req.cookies);
   if (!token) throw new UnauthenticatedError('authentication invalid');
-  next();
+  try {
+    const { userId, role } = verifyJWT(token);
+    console.log(userId, role);
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError('authentication invalid');
+  }
 }
