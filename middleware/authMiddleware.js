ab44f6ac247e50386f6788c2bc8170eb9a6347a1
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-02-19 17:38:46 +0530

    fix authenticateUser function

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,13 +3,16 @@ import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = async (req, res, next) => {
   const { token } = req.cookies;
-  // console.log(req.cookies);
-  if (!token) throw new UnauthenticatedError('authentication invalid');
+  if (!token) {
+    throw new UnauthenticatedError('authentication invalid');
+  }
+
   try {
     const { userId, role } = verifyJWT(token);
-    console.log(userId, role);
+    req.user = { userId, role };
+    // console.log(userId, role);
     next();
   } catch (error) {
     throw new UnauthenticatedError('authentication invalid');
   }
-}
+};
