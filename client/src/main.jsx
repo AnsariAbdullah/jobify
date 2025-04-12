2fad6ce3666eb7ce1db1946a9864d31fa47979a9
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-04-12 16:32:05 +0530

    clean up

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
-import customFetch from './utils/customFetch.js';
 
-const resp = await customFetch.get('/test');
-console.log(resp);
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
