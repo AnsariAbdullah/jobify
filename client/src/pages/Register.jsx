fb77afc1067ab90fbf2f2872eef358c4521a4d5e
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-04-15 16:42:06 +0530

    add toasts

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,17 +2,17 @@ import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { Logo, FormRow } from '../components';
 import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
 
 export const actions = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log(formData);
-  console.log(data);
   try {
     await customFetch.post('/auth/register', data);
+    toast.success('Registration sucessful');
     return redirect('/login');
   } catch (error) {
-    console.log(error);
+    toast.error(error?.response?.data?.msg);
     return error;
   }
 };
