480b0280a8fafdb398953c145bb441d7b216db01
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-05-10 17:03:11 +0530

    add addJob structure and select input

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,4 +1,4 @@
-import { FormRow } from '../components';
+import { FormRow, FormRowSelect } from '../components';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { useOutletContext } from 'react-router-dom';
 import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
@@ -23,6 +23,18 @@ const AddJob = () => {
             labelText='job location'
             name='jobLocation'
             defaultValue={user.location}
+          />
+          <FormRowSelect
+            name='jobStatus'
+            labelText='job status'
+            list={Object.values(JOB_STATUS)}
+            defaultValue={JOB_STATUS.PENDING}
+          />
+          <FormRowSelect
+            name='jobType'
+            labelText='job type'
+            list={Object.values(JOB_TYPE)}
+            defaultValue={JOB_TYPE.FULL_TIME}
           />
           <button
             type='submit'
