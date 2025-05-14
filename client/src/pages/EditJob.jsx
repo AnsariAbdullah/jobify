71a456273d772bb19c62c458c4c110c8e47f14f0
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-05-14 20:44:48 +0530

    add loader with action function structure

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,4 +1,34 @@
+import { FormRow, FormRowSelect } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { useLoaderData } from 'react-router-dom';
+import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
+import { Form, useNavigation, redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+
+export const loader = async ({ params }) => {
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`);
+    return data;
+  } catch (error) {
+    toast.error(error.response.data.msg);
+    return redirect('/dashboard/all-jobs');
+  }
+};
+
+export const action = async () => {
+  return null;
+};
+
 const EditJob = () => {
+  // we can use useParams hook to get params as shown below
+  // const params = useParams();
+  // console.log(params);
+  // but since we get it in loader we doon't need to use this hook
+
+  const { job } = useLoaderData();
+  console.log(job);
+
   return <div>EditJob</div>;
 };
 export default EditJob;
