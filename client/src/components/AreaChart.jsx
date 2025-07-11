9d7f3eaadac82da9d35f6e06a4f2aa7756474de4
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-07-11 17:14:07 +0530

    add AreaChart and BarChart

--- client/src/components/AreaChart.jsx
+++ client/src/components/AreaChart.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
 
-function AreaChart() {
-  return <p>AreaChart</p>;
+function AreaChartComponent({ data }) {
+  return (
+    <ResponsiveContainer width='100%' height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='date' />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type='monotone' dataKey='count' stroke='#2cb1bc' fill='#bef8fd' />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 }
 
-export default AreaChart;
+export default AreaChartComponent;
