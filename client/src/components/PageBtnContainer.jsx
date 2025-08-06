18d6e97050c939806e90588867897624562b222b
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-08-06 15:56:41 +0530

    add buttons

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -9,9 +9,31 @@ function PageBtnContainer() {
     data: { numOfPages, currentPage },
   } = useAllJobsContext();
 
-  console.log(numOfPages, currentPage);
+  const pages = Array.from({ length: numOfPages }, (_, index) => index + 1);
+  console.log(pages);
 
-  return <h1>PageBtnContainer</h1>;
+  return (
+    <Wrapper>
+      <button className='btn prev-btn'>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className='btn-container'>
+        {pages.map((pageNumber) => (
+          <button
+            className={`btn page-btn ${pageNumber === currentPage && 'active'}`}
+            key={pageNumber}
+          >
+            {pageNumber}
+          </button>
+        ))}
+      </div>
+      <button className='btn next-btn'>
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
 }
 
 export default PageBtnContainer;
