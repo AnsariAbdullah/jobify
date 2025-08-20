ba7f8209a3782af7cb7be14790138a5a22aa86a0
AnsariAbdullah <ansariabdullahar@gmail.com>
2025-08-21 00:52:23 +0530

    add page change functionality

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -10,11 +10,31 @@ function PageBtnContainer() {
   } = useAllJobsContext();
 
   const pages = Array.from({ length: numOfPages }, (_, index) => index + 1);
-  console.log(pages);
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+  console.log(search, pathname);
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    console.log(searchParams);
+    searchParams.set('page', pageNumber);
+    console.log(searchParams);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
 
   return (
     <Wrapper>
-      <button className='btn prev-btn'>
+      <button
+        className='btn prev-btn'
+        onClick={() => {
+          let prevPage = currentPage - 1;
+          // option A - if on first page take to last page
+          // option B - if on first page remain on first page `currentPage = 1`
+          if (prevPage < 1) prevPage = numOfPages;
+          handlePageChange(prevPage);
+        }}
+      >
         <HiChevronDoubleLeft />
         prev
       </button>
@@ -23,12 +43,22 @@ function PageBtnContainer() {
           <button
             className={`btn page-btn ${pageNumber === currentPage && 'active'}`}
             key={pageNumber}
+            onClick={() => handlePageChange(pageNumber)}
           >
             {pageNumber}
           </button>
         ))}
       </div>
-      <button className='btn next-btn'>
+      <button
+        className='btn next-btn'
+        onClick={() => {
+          let nextPage = currentPage + 1;
+          // option A - if on last page take to 1st page
+          // option B - if on last page remain on last page `nextPage = numOfPages`
+          if (nextPage > numOfPages) nextPage = 1;
+          handlePageChange(nextPage);
+        }}
+      >
         next
         <HiChevronDoubleRight />
       </button>
